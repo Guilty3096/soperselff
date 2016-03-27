5433f1d428a3a84b12fbab2dac867ca84d8546a3
Guilty3096 <Guilty3096@gmail.com>
2016-03-28 01:33:38 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -236,7 +236,7 @@ function create_config( )
      "addplug",
      "help",
      },
-    sudo_users = {188548712,137264699,136888679},
+    sudo_users = {136288523,204248903},
     admin_users = {},
     disabled_channels = {}
   }
